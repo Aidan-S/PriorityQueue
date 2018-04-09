bab2e9f5490f726f32ae47f8e4164aca9421df90
scannella <scannella@bishopireton.org>
2018-04-09 12:53:30 -0400

    What.....

---
+++ src/priorityQueue.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class priorityQueue {
+	
+	
+	ArrayList<Integer> list;
+	
+	public priorityQueue() {
+		list = new ArrayList<Integer>();
+	}
+	
+	public void add(int a) {
+		int index = list.size()/2;
+		if(list.get(index) > a) {
+			
+		}else {
+			
+		}
+		
+	}
+	
+	
+	
+	
+	public static void main (String args[]) {
+		System.out.println(0/2);
+	}
+	
+}
